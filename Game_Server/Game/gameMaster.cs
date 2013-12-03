5c90001259296dd5389a537381df063a26a3277d
phillipv20 <phillip.vinson@bobcats.gcsu.edu>
2013-12-03 14:57:48 -0500

    Updated Game Master
    Added lots of new questions and updated the maxQuestions variable.

--- Game_Server/Game/gameMaster.cs
+++ Game_Server/Game/gameMaster.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,7 +14,7 @@ namespace Game_Server.Game
 
         public static String updatedCorrectAnswer = null;
        
-        private static int numberOfQuestions = 12;
+        private static int numberOfQuestions = 30;
 
         //create an array for checking to make sure no questions are repeated and initialize to false
         private static bool[] boolArray = new bool[numberOfQuestions];
@@ -149,15 +149,178 @@ namespace Game_Server.Game
                     correctAnswer = "C";
                     break;
 
-                default:
-                    question = "Who teaches Networking at GCSU?";
-                    answers = "A: Bill Gates"
-                            + Environment.NewLine + "B: Dr. Liu"
-                            + Environment.NewLine + "C: Dr. Yao"
-                            + Environment.NewLine + "D: Morgan Freeman";
+                case 12:
+                    question = "In the 1950s, the NFL experimented with what new color for their footballs?";
+                    answers = "A: Black"
+                            + Environment.NewLine + "B: Blue"
+                            + Environment.NewLine + "C: Orange"
+                            + Environment.NewLine + "D: White";
+                    correctAnswer = "D";
+                    break;
+
+                case 13:
+                    question = "What does the abbreviation CSS stand for?";
+                    answers = "A: Cascading Style Sheets"
+                            + Environment.NewLine + "B: Cat Style Sheets"
+                            + Environment.NewLine + "C: Correct Style Sheets"
+                            + Environment.NewLine + "D: Compiled Style Sheets";
+                    correctAnswer = "A";
+                    break;
+
+                case 14:
+                    question = "What three colours does Asparagus come in?";
+                    answers = "A: Green, White, Yellow"
+                            + Environment.NewLine + "B: Green, White, Purple"
+                            + Environment.NewLine + "C: Gray, White, Purple"
+                            + Environment.NewLine + "D: Black, Blue, Pink";
                     correctAnswer = "B";
                     break;
 
+                case 15:
+                    question = "What do the letters LG stand for in the name of the international electronics and chemical brand LG?";
+                    answers = "A: Lucky-Goldstar"
+                            + Environment.NewLine + "B: Lifes-Good"
+                            + Environment.NewLine + "C: Little-GAP"
+                            + Environment.NewLine + "D: Little-Graphics";
+                    correctAnswer = "A";
+                    break;
+
+                case 16:
+                    question = "Who developed the World Wide Web?";
+                        answers = "A: Bill Gates"
+                            + Environment.NewLine + "B: Steve Jobs"
+                            + Environment.NewLine + "C: Steve Wasnac"
+                            + Environment.NewLine + "D: Tim Berners-Lee";
+                    correctAnswer = "D";
+                    break;
+
+                case 17:
+                    question = "Who is the inventor of Coca Cola?";
+                    answers = "A: Dr. Liu"
+                        + Environment.NewLine + "B: Mike Cola"
+                        + Environment.NewLine + "C: John Pemerton"
+                        + Environment.NewLine + "D: Coco Coola";
+                    correctAnswer = "C";
+                    break;
+
+                case 18:
+                    question = "How did Alice enter Wonderland?";
+                    answers = "A: Through a Door"
+                        + Environment.NewLine + "B: Through a Rabbit Hole"
+                        + Environment.NewLine + "C: Through a Tornado"
+                        + Environment.NewLine + "D: Jumping into the Event Horizon of a Black Hole";
+                    correctAnswer = "B";
+                    break;
+
+                case 19:
+                    question = "What country did french fries come from?";
+                    answers = "A: United States"
+                        + Environment.NewLine + "B: France"
+                        + Environment.NewLine + "C: Belgium"
+                        + Environment.NewLine + "D: Germany";
+                    correctAnswer = "C";
+                    break;
+
+                case 20:
+                    question = "Who inveneted JAVA?";
+                    answers = "A: Bill Gates"
+                        + Environment.NewLine + "B: James Gosling"
+                        + Environment.NewLine + "C: Oracle"
+                        + Environment.NewLine + "D: Paul Allen";
+                    correctAnswer = "B";
+                    break;
+
+                case 21:
+                    question = "Worlds first computer programmer?";
+                    answers = "A: Bill Gates"
+                        + Environment.NewLine + "B: James Gosling"
+                        + Environment.NewLine + "C: Ada Lovelace"
+                        + Environment.NewLine + "D: Richard Swine";
+                    correctAnswer = "C";
+                    break;
+
+                case 22:
+                    question = "What was the name of the Earth's first artificial satellite?";
+                    answers = "A: The Moon"
+                        + Environment.NewLine + "B: Maverick I"
+                        + Environment.NewLine + "C: Lovelace II"
+                        + Environment.NewLine + "D: Sputnik I";
+                    correctAnswer = "D";
+                    break;
+
+                case 23:
+                    question = "What is a male witch called?";
+                    answers = "A: Warlcok"
+                        + Environment.NewLine + "B: Witch"
+                        + Environment.NewLine + "C: Wizard"
+                        + Environment.NewLine + "D: Shaman";
+                    correctAnswer = "A";
+                    break;
+
+                case 24:
+                    question = "What does the abbreviation USB stand for?";
+                    answers = "A: None of the below"
+                        + Environment.NewLine + "B: Unsorted Serial Bus"
+                        + Environment.NewLine + "C: Universal Serial Bus"
+                        + Environment.NewLine + "D: Universal Sectioned Bus";
+                    correctAnswer = "C";
+                    break;
+
+                case 25:
+                    question = "Which dinosaur never really existed?";
+                    answers = "A: Mastadon"
+                        + Environment.NewLine + "B: Brachiosaurus"
+                        + Environment.NewLine + "C: Tyrannosaurus Rex"
+                        + Environment.NewLine + "D: Brontosaurus";
+                    correctAnswer = "D";
+                    break;
+
+                case 26:
+                    question = "What do ladybugs feed on during hibernation?";
+                    answers = "A: Leaves"
+                        + Environment.NewLine + "B: Stored Mucus"
+                        + Environment.NewLine + "C: Stored Fat"
+                        + Environment.NewLine + "D: Table Scraps";
+                    correctAnswer = "C";
+                    break;
+
+                case 27:
+                    question = "How many Harry Potter movies are there?";
+                    answers = "A: 6"
+                        + Environment.NewLine + "B: 7"
+                        + Environment.NewLine + "C: 8"
+                        + Environment.NewLine + "D: 22";
+                    correctAnswer = "C";
+
+                    break;
+
+                case 28:
+                    question = "Which of the following is most closely related to a shark?";
+                    answers = "A: Barracuda"
+                        + Environment.NewLine + "B: Bass"
+                        + Environment.NewLine + "C: Stingray"
+                        + Environment.NewLine + "D: Tuna";
+                    correctAnswer = "C";
+
+                    break;
+
+                case 29:
+                    question = "In computer machinery what does the acronym HSF stand for?";
+                    answers = "A: Heap Sorting Function"
+                            + Environment.NewLine + "B: Heat Sink Funnel"
+                            + Environment.NewLine + "C: Header Style format"
+                            + Environment.NewLine + "D: Heat Sink and Fan";
+                    correctAnswer = "D";
+                    break;
+
+                default:
+                    question = "Who teaches Systems and Network Programming?";
+                    answers = "A: Dr. Phelps"
+                            + Environment.NewLine + "B: Dr. Liu"
+                            + Environment.NewLine + "C: Dr. Goette"
+                            + Environment.NewLine + "D: Dr. Who";
+                    correctAnswer = "C";
+                    break;
             }
 
             setNewQuestion(question);
